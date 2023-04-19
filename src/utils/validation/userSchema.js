f0dcb82b54e19407f1b8666fd5e2ac2a4f43953d
Aya Qunoo <qunooa@gmail.com>
2023-04-19 14:09:55 +0200

    handling errors Relates #5

--- src/utils/validation/userSchema.js
+++ src/utils/validation/userSchema.js
@@ -2,10 +2,11 @@
 const joi = require('joi');
 
 const signUpSchema = joi.object({
-  username: joi.string().min(3).max(20).required()
+  username: joi.string().min(2).max(20).required()
     .alphanum(),
   email: joi.string().email().required(),
-  password: joi.string().required().min(8),
+  password: joi.string().required().regex(/^[a-zA-Z0-9]{5,30}$/),
+  confirmedPassword: joi.ref('password'),
 });
 const loginSchema = joi.object({
   email: joi.string().email().required(),
